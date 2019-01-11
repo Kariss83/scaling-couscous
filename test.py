959d4eeb2919b1c78f9aece9c2601efc8db33581
kariss83 <vache.romain@gmail.com>
2019-01-11 16:24:53 +0100

    working version of populate prod, cat and tags

--- test.py
+++ test.py
@@ -26,6 +26,7 @@ class Fetcher:
         self.json_response = None
         self.criteria = None
         self.urlapi = "https://fr.openfoodfacts.org/cgi/search.pl"
+        self.tags_as_set = None
     
     def create_table(self, connector):
         """This method will create the DB.""" 
@@ -48,7 +49,7 @@ class Fetcher:
         connector.db.query('DROP TABLE IF EXISTS Tags')
         connector.db.query("""CREATE TABLE Tags (
 	        id INT PRIMARY KEY AUTO_INCREMENT,
-            name VARCHAR(50)  NOT NULL)"""
+            name VARCHAR(100)  NOT NULL)"""
         )
         connector.db.query('DROP TABLE IF EXISTS Product_has_tag')
         connector.db.query("""CREATE TABLE Product_has_tag (
@@ -74,12 +75,15 @@ class Fetcher:
         self.response = requests.get(self.urlapi, params=self.criteria)
 
 
-    def populate(self, category, connector):
+    def populate_products(self, category, connector):
         """This method will populate the DB with data from the API"""
         self.json_response = self.response.json()['products']
         for rec in self.json_response:
             try:
-                category_id = CATEGORIES.index(category)
+                category_id_raw = connector.db.query("""SELECT id FROM Categories
+                WHERE name = :category""", category=category)
+                print(category_id_raw)
+                print(category_id_raw[0].id)
                 product_name = rec['product_name_fr']
                 nutrition_grade = rec['nutrition_grades_tags'][0]
                 stores_raw = rec['stores']
@@ -94,10 +98,40 @@ class Fetcher:
                     tags, category_id, nutrigrade, stores) VALUES(NULL, :name, 
                     :url, :tags, :category_id, :nutrigrade, :stores)""",
                     name=product_name, url=url, 
-                    tags=str(tags_raw), category_id=category_id, 
+                    tags=str(tags_raw), category_id=category_id_raw[0].id, 
                     nutrigrade=nutrition_grade, stores=stores_raw)
             except KeyError:
                 print("value missing")
+
+    def populate_categories(self, connector):
+        """This method will populate the DB with data from the API"""
+        try:
+            for cat in CATEGORIES:
+                connector.db.query("""INSERT INTO Categories(name) VALUES(
+                :name)""", name=cat)
+        except KeyError:
+            print("Value missing")
+    
+    def populate_tags(self, connector):
+        try:
+            unique_tags_set = set()
+
+            all_prod = connector.db.query("""SELECT * FROM Products""")
+            for prod in all_prod:
+                tags_as_str = prod.tags.split('[')[-1].split((']')[0])
+                tags_as_list = tags_as_str[0].split(',')
+                for item in tags_as_list:
+                    item.replace(' ', '')
+                prod_tag_set = set(tags_as_list)
+                unique_tags_set = prod_tag_set | unique_tags_set
+            for unique_tag in unique_tags_set:
+                connector.db.query("""INSERT INTO Tags(name) VALUES(
+                    :name)""", name=unique_tag)
+        except KeyError:
+
+            pass
+
+
 
 if __name__ == '__main__':
     fetcher = Fetcher()
@@ -106,4 +140,6 @@ if __name__ == '__main__':
     fetcher.create_crits("categories", "contains", "pizza", 1000, 
     "unique_scans_n")
     fetcher.request()
-    fetcher.populate("pizza", connector)
+    fetcher.populate_categories(connector)
+    fetcher.populate_products("pizza", connector)
+    fetcher.populate_tags(connector)
